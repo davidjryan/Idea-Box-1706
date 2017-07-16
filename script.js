d0d2a3154f2405684b2665b2c077c5410603ae3d
David Ryan <djrdev@icloud.com>
2017-07-16 14:10:22 -0600

    eventlisteners & card creation

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
+$('.save-button').on('click', cardCreation);
+$('.main-container').on('click', '.card-delete-button', deleteCard);
+$('.main-container').on('click', '.card-quality-up', upVote);
+$('.main-container').on('click', '.card-quality-down', downVote);
+$('.input-search').on('keyup', filterInput);
 
+function IdeaCard(title, body) {
+  this.title = title,
+  this.body = body,
+  this.quality = "Swill",
+  this.id = Date.now()
+}
+
+function cardCreation(event) {
+  event.preventDefault();
+  var title = $('.input-title').val();
+  var body = $('.input-body').val();
+  var Idea = new IdeaCard(title, body)
+
+  cardHTML(Idea)
+  clearInputs();
+}
+
+function cardHTML(object) {
+  $('.main-container').prepend(`<article>
+    <input type="text" class="card-title" placeholder="Example Idea 1" value="${object.title}">
+    <button class="card-delete-button"></button>
+    <textarea class="card-body" name="" id="" cols="30" rows="3">${object.body}</textarea>
+    <section class="card-quality-container">
+      <button class="card-quality-up"></button>
+      <button class="card-quality-down"></button>
+      <p class="card-quality-text">quality: ${object.quality}</p>
+    </section>
+    <hr />
+  </article>`)
+}
+
+
+function clearInputs() {
+  $('.input-title').val('');
+  $('.input-body').val('');
+}
+
+function filterInput() {
+  var searchInput = $(this).val();
+}
+
+function deleteCard() {
+  $(this).closest('article').remove();
+}
+
+function downVote() {
+
+}
+
+function upVote() {
+
+}
+
+function localStorageInterface() {
+
+}
